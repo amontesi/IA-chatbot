c28ca18d4940cccb81a7df0ada819db708c2c3b2
Agustin <agustmontesi@gmail.com>
2023-05-22 22:57:55 -0300

    first commit

---
+++ src/app/api/generate/route.js
@@ -0,0 +1,34 @@
+import { error } from "console";
+import { NextResponse } from "next/server";
+import { Configuration, OpenAIApi } from "openai";
+
+const configuration = new Configuration({
+  apiKey: "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",
+});
+
+if (!configuration.apiKey) {
+  throw new Error("OPENAI_API_KEY is required");
+}
+
+const openai = new OpenAIApi(configuration);
+
+export async function POST(req) {
+  const body = await req.json();
+
+  if (!body.prompt || !body.prompt.length === 0) {
+    return NextResponse.error(new Error("Prompt is required"), {
+      status: 400,
+    });
+  }
+  try {
+    const response = await openai.createCompletion({
+      prompt: body.prompt,
+      model: "text-davinci-003",
+      temperature: 0.7,
+      max_tokens: 120,
+    });
+    return NextResponse.json(response.data.choices[0].text);
+  } catch (e) {
+    return NextResponse.error(error, { status: 500 });
+  }
+}
